8c90d98f7cdeb0393467eb4a7aa7558d389a7d5f
Rafael Sa <rafael_jdsb@hotmail.com>
2021-01-10 17:03:52 -0300

    Event producer service

---
+++ event-producer-service/src/test/java/br/com/iotplatform/eventproducerservice/EventProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iotplatform.eventproducerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProducerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
